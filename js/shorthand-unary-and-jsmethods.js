fe7caa2450acefcf6be47f9136029cf6c8775ddd
Sean Lewis <sean.lewis018@gmail.com>
2021-04-16 09:14:47 -0500

    ops and shorthand

---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// SHORTHAND ASSIGNMENT OPERATORS
+
+// += / -=
+
+var name = "Sean";
+
+//name = name+ " " + "Lewis";
+//console.log(name);
+
+name += " Lewis";
+console.log(name);
+
+//console.log(name -= "Lewis";  //NaN
+
+var daysRemainingInCohort = 114;
+
+daysRemainingInCohort -= 4;
+
+console.log(daysRemainingInCohort);
+
+// MINI EXERCISE
+
+
+// TODO: Refactor the mini exercise we did earlier on arithmetic operator to use variables and related shorthand assignment operators, then log out the result
+// var result = 5;
+// console.log(result += 1);
+
+var result = 1;
+console.log(result += 2);
+
+
+ // UNARY
+
+var makeItANumber ="5";
+
+console.log()
+
+// INCREMENT/DECREMENT
+
+// post-increment (num++)
+
+var num = 10;
+
+console.log(num++); // num += 1
+console.log(num);
+
+// JS BUILT-IN METHODS
+
+console.log()
+
+// toString();
+
+var num = 5 * 5;
+
+console.log(typeof num.toString());
+console.log(typeof num);
+num = num.toString();
+
+
+
+
+
