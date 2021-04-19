0fa8cc70505f6845e1006c2ffe6cbb00e880e33e
Sean Lewis <sean.lewis018@gmail.com>
2021-04-19 08:24:40 -0500

    functions

---
+++ js/functions-lecture.js
@@ -0,0 +1,30 @@
+//"use strict";
+
+//var num = 5
+
+//num = num + 5;
+
+//console.log(num);
+//creating an interface for other code to use without knowing what happens inside add()
+//function add(num1, num2){ // <-- parameters here
+    //return num1 + num2;
+//}
+
+//console.log("The result of add() is: " + add()); // <-- arguments here
+
+//-------------Multiplication
+
+//function multiply(num1, num2){
+    //return num1 * num2;
+//}
+
+//console.log("The product of multiply() is: " * multiply());
+
+//--------------Divide
+
+//function divide(num1, num2){
+    //return num1 / num2;
+//}
+
+//console.log("The result of divide() is: " / divide());
+
