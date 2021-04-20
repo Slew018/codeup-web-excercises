049225f33fe92e638a0cbd56ca44ebbc0ea8751e
Sean Lewis <sean.lewis018@gmail.com>
2021-04-20 10:02:09 -0500

    update all

---
+++ js/conditions.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function analyzeColor()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"use strict";
+
+function calculateTotal(luckyNumber, originalPrice) {
+    var originalPrice;
+    var  result;
+
+    switch (luckyNumber) {
+        case 1:
+            var = result = .10
+            break;
+        case 1:
+            var = result = .10
+            break;
+        case 1:
+            var = result = .10
+            break;
+        case 1:
+            var = result = .10
+            break;
+        case 1:
+            var = result = .10
+            break;
+    }
+}
