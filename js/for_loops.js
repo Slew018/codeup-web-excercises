6c9bf3c92e4f94bf6d53586152747fd6795f2fb1
Sean Lewis <sean.lewis018@gmail.com>
2021-04-23 16:32:43 -0500

    loops

---
+++ js/for_loops.js
@@ -0,0 +1,98 @@
+"use strict";
+
+
+// for(var i = 0; i <= 10; i++){
+//     // code to be executed
+//     console.log('hello world' + i);
+// }
+
+// for(var i = -10; i >= 0; i++){
+//     // code to be executed
+//     console.log(`hello world ${i}`);
+// }
+
+// for (var x  = 10; x <= 25; x++){
+//     console.log(x * 2);
+// }
+//
+// var carton = 0;
+// for(var eggs = 1; eggs <= 72; eggs++){
+//     if(72 % 12 == 0){
+//         carton ++;
+//         console.log(`We've filled ${carton} amount of egg cartons with ${eggs} number of eggs`);
+//     }
+//
+// }
+
+//
+//
+// for(var i =0; i <= 10; i++){
+//     console.log(`first: ${i}`); //0
+//     continue // ---> exit current iteration (continues) and move onto the next iteration
+//     i = i*2;
+//     console.log(`second ${i}`); // 0 // 2
+// }
+
+// for (var counter = 1; counter <= 5; counter++){
+//     console.log(counter);
+//     // nested loop: another for loop
+//     // // count from 5 to 1
+//     // // log those numbers
+//     if(counter === 5){
+//         for(var counterTwo = 5; counterTwo > 0; counterTwo--){
+//             console.log(counterTwo);
+//         }
+//     }
+// }
+
+// for (let i = 0; i <= 30; i++){
+//     console.log(i);
+//     if(i % 2  === 0){
+//         console.log(i + " add 2 to even number"); //even number adding 2
+//     }else{
+//         if(i % 2 !== 0)
+//         console.log("Odd");
+//     }
+//     }
+//
+
+
+
+
+
+
+
+
+
+// // TODO: loop through numbers 1 - 40. Print "marco" if it's divisible by 3. Print "polo" if it's divisible by 5.
+//  If it's neither print "I'm not playing"
+//  Print "marco polo" if it's divisible by 3 and 5.
+
+
+// for(var i = 0; i <= 40; i++){
+//
+//      console.log('hello world' + i);
+//
+// if(72 % 12 == 0) {
+//     carton++;
+
+
+
+// function showMultiplicationTable(num){
+//     console.log("multiplication table for ");
+//     for(let i = 1; i <= 10; i++){
+//         console.log(`${num} * ${i} === ${num * i}`);
+//     }
+// }
+// showMultiplicationTable(7);
+
+// for(let i = 1; i <= 10; i++){
+//     let random = Math.floor(x: Math.random() * (200- 20 + 1) + 20);
+//     if (i % 2 === 0){
+//         console.log(i + " even numbers");
+//     }
+// }
+
+// for(let i = 100; i <= 0; i-= 5){
+//     console.log(i);
+// }
