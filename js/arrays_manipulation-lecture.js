7abc5d7aff68e1f7c87e89797d4004c0cf19c4ba
Sean Lewis <sean.lewis018@gmail.com>
2021-04-26 14:55:56 -0500

    more arrays

---
+++ js/arrays_manipulation-lecture.js
@@ -0,0 +1,50 @@
+(function(){
+    "use strict";
+
+    //TODO:
+    // -> .length IMPORTANT
+    // -> .forEach()
+    // -> FOR LOOP
+
+    // var days = ['Monday', 'Tuesday', 'Wednesday'];
+    // days.push('Thursday');
+    //
+    // var removedElement = days.pop();
+    //
+    // console.log(days);
+    // console.log('The removed element was: ' + removedElement);
+
+
+    // console.log(days.length);
+    // var newLength = days.push('Thursday');
+    // console.log(newLength);
+
+    //TODO: MINI-EXERCISE
+    // -> Create an array with the remaining days of the week
+    // -> Call .forEach() on that NEW array
+    // -> Inside the inline function you pass into .forEach()
+    //  -> add the remainingDay of the week to the old array
+    //  -> When the loop completes, console.log() the completed day of the Week
+
+    // var days = ['Monday', 'Tuesday', 'Wednesday'];
+    // var missingDays = ['Thursday', 'Friday', 'Saturday', 'Sunday'];
+    //
+    // missingDays.forEach(function(day){
+    //     days.push(day);
+    // });
+    //     console.log(days);
+
+
+    var numArray = [1, 2, 3, 4, 5,];
+
+    console.log(numArray.indexOf(1));
+
+    numArray.unshift();
+
+    console.log(numArray);
+
+    numArray.sort();
+
+
+
+})();
