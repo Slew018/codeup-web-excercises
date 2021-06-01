3c4a1843ea800c43cfe2fbaf38b1369fe0dcd360
Sean Lewis <sean.lewis018@gmail.com>
2021-06-01 15:51:57 -0500

    jquery

---
+++ js/jquery_js.js
@@ -0,0 +1,28 @@
+$('#all-caps').hide();
+
+$('span').hide();
+
+$('.show-me').show();
+
+$(document).ready(function(){
+ $('.orange').css('background-color', 'orange').fadeToggle(1000).fadeToggle(1000).fadeToggle(1000).show();
+});
+
+window.onload = function(){
+ $('.pink').css('background-color', 'pink').fadeToggle(1000).fadeToggle(1000).fadeToggle(1000).show();
+};
+
+
+// $('img').each(function(i, item) {
+//     let imgHeight = $(item).height();
+//     let divHeight = $(item).parent().height();
+//     if(imgHeight<divHeight){
+//         $(item).css({'width': 'auto', 'height': '100%'});
+//
+//     }
+// });
+
+ $('img').css({'width': 'auto', 'height': '100%'});
+
+
+
